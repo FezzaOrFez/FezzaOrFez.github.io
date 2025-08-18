0c30d42f0ea60aa4606d7f7d6da36fe70b24b4d5
JayFerrier <86517374+FezzaOrFez@users.noreply.github.com>
2025-08-18 15:47:03 +0100

    completed version 1.0

--- scripts.js
+++ scripts.js
@@ -6,14 +6,12 @@ function openProject(id) {
         var height = document.getElementById(id+"Dropdown").clientHeight
         height = height + (20)
         document.getElementById(id+"Container").style.marginBottom = height + "px";
-        document.getElementById(id+"Overlay").style.height = 100+"%";
 
     } else{
         document.getElementById(id+"Dropdown").style.transition = "top .4s ease-in-out, opacity 1s ease";
         document.getElementById(id+"Dropdown").style.top = 0+"%";
         document.getElementById(id+"Dropdown").style.opacity = 0+"%";
         document.getElementById(id+"Container").style.marginBottom = 5+"%";
-        document.getElementById(id+"Overlay").style.height = 0+"%";
     }
 }
 
@@ -28,7 +26,5 @@ function closeAll(){
     for (let i = 0; i < containers.length; i++){
         containers[i].style.marginBottom = 5+"%";
     }
-    for (let i = 0; i < overlays.length; i++){
-        overlays[i].style.height = 0+"%";
-    }
+
 }
